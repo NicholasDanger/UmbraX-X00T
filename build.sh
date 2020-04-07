1fabdb581adec23f1832df167b7d257da6322695
DarknessKiller <kaelvinchen2@gmail.com>
2020-04-07 11:13:43 +0800

    Xenium R3.1 Release

--- build.sh
+++ build.sh
@@ -17,6 +17,6 @@
 	echo "Compile Done"
 	echo ""
 	cd $KERNELDIR/built
-	zip -r Xenium-R3-Release-X00T-`date +%Y%m%d_%H%M`.zip * -x "kernel_output.txt" "*.zip"
+	zip -r Xenium-R3.1-Release-X00T-`date +%Y%m%d_%H%M`.zip * -x "kernel_output.txt" "*.zip"
 	cd $KERNELDIR/
 	echo "Zip Done"
--- built/anykernel.sh
+++ built/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Xenium R2 - Adapt & Improve
+kernel.string=Xenium R3.1 - Adapt & Improve
 do.devicecheck=1
 do.modules=0
 do.systemless=1
