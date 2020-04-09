57cfdff1e438ac3d12681fb8e8249701c47702b5
DarknessKiller <kaelvinchen2@gmail.com>
2020-04-09 13:25:10 +0800

    Xenium R4 Release * Rebased To CAF Q [Thanks SonicBSV & Anomalchik] * Miui Booted Fine On Clang * Fixed Audio/Camera Issue On OOS

--- built/anykernel.sh
+++ built/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Xenium R3 - Adapt & Improve
+kernel.string=Xenium R4 - Adapt & Improve
 do.devicecheck=1
 do.modules=0
 do.systemless=1
