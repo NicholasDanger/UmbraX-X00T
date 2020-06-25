95fa98bc08b52ccae92f38f27e067fa432ead45a
DarknessKiller <kaelvinchen2@gmail.com>
2020-06-25 18:04:23 +0800

    Xenium R5 Release

--- built/anykernel.sh
+++ built/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Xenium R4 - Adapt & Improve
+kernel.string=Xenium R5 - Long Term Version
 do.devicecheck=1
 do.modules=0
 do.systemless=1
